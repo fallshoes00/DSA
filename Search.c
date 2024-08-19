bf3cb1f7f5552eec2e4df1da07551cd6fbf211ef
fallshoes00 <87844088+fallshoes00@users.noreply.github.com>
2024-08-19 23:59:41 +0800

    Update Search.c

--- Search.c
+++ Search.c
@@ -1,73 +1,57 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-// Linear Search
-int linearSearch(int arr[], int n, int x) {
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == x)
+int min(int a, int b){
+    if (a>b) return b;
+    else return a;
+}
+
+int linear_search(int arr[], int size, int target){
+    for(int i =0; i<size; i++){
+        if(arr[i] == target)
             return i;
     }
-    return -1; // Not found
+    return -1;
 }
 
-// Binary Search
-int binarySearch(int arr[], int low, int high, int x) {
-    while (low <= high) {
-        int mid = low + (high - low) / 2;
-        // x剛好在中間，直接return
-        if (arr[mid] == x)
+int binary_search(int arr[], int low, int high, int target){
+    
+    while(low<=high){
+        int mid = low + (high-low)/2;
+        if (arr[mid] == target) 
             return mid;
-        // x大，忽略左邊；x小，忽略右邊
-        if (arr[mid] < x)
-            low = mid + 1;
+        
+        if(arr[mid] < target){
+            low = mid+1;
+        }
         else
-            high = mid - 1;
+            high = mid-1;
     }
-    return -1; // Not found
+    return -1;
 }
 
-// Exponential Search
-int exponentialSearch(int arr[], int n, int x) {
-    // 依序從第1、2、4、8、16、32...去確認目標大略位置
-    // 找到之後再啟動Binary Search去找到確切位置
-    // 目標直接在第 0個
-    if (arr[0] == x)
-        return 0;
-
-    // 找大略位置
-    int i = 1;
-    while (i < n && arr[i] <= x)
-        i = i * 2;
-
-    // 找確切位置，fmin取兩者較小的數值，確保search不會超出array range
-    return binarySearch(arr, i / 2, fmin(i, n - 1), x);
+int exponential_search(int arr[], int n, int target ){
+    if (arr[0] == target ) return 0;
+    
+    int i=1;
+    while(arr[i] < target){
+        i *= 2;
+    }
+    return binary_search(arr, i/2, min(i, n), target);
 }
 
-int main() {
-    int arr[] = {2, 3, 4, 10, 40, 50, 70, 80, 90};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int x = 10;
 
-    // Test Linear Search
-    int result = linearSearch(arr, n, x);
+// Driver code
+int main(void)
+{
+    int arr[] = { 2, 3, 4, 10, 40,189,491,5261};
+    int target = 189;
+    int size = sizeof(arr)/sizeof(arr[0]);
+    //int result = linear_search(arr, size, target);
+    //int result = binary_search(arr, 0, size-1, target);
+    int result = exponential_search(arr, size, target);
     if (result == -1)
-        printf("目標沒有在array裡面(Linear Search)\n");
+        printf("Element is not present in array");
     else
-        printf("目標在第%d個位置(Linear Search)\n", result);
-
-    // Test Binary Search
-    result = binarySearch(arr, 0, n - 1, x);
-    if (result == -1)
-        printf("目標沒有在array裡面(Binary Search)\n");
-    else
-        printf("目標在第%d個位置(Binary Search)\n", result);
-
-    // Test Exponential Search
-    result = exponentialSearch(arr, n, x);
-    if (result == -1)
-        printf("目標沒有在array裡面(Exponential Search)\n");
-    else
-        printf("目標在第%d個位置(Exponential Search)\n", result);
-
+        printf("Element is present at index %d", result);
     return 0;
 }
